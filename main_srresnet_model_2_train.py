f3730cb84d8081e99d55e174c62ce7027c61b8c6
Arunav Shandilya <arunavshandilya96@gmail.com>
2019-06-22 12:42:35 +0530

    Update main_srresnet_model_2_train.py

--- main_srresnet_model_2_train.py
+++ main_srresnet_model_2_train.py
@@ -30,7 +30,7 @@ parser.add_argument("--batchSize", type=int, default=16, help="training batch si
 parser.add_argument("--nEpochs", type=int, default=500, help="number of epochs to train for")
 parser.add_argument("--lr", type=float, default=1e-4, help="Learning Rate. Default=1e-4")
 parser.add_argument("--step", type=int, default=200, help="Sets the learning rate to the initial LR decayed by momentum every n epochs, Default: n=500")
-parser.add_argument("--cuda", action="store_true", help="Use cuda?")
+parser.add_argument("--cuda", default = True, action="store_true", help="Use cuda?")
 parser.add_argument("--resume", default="", type=str, help="Path to checkpoint (default: none)")
 parser.add_argument("--start-epoch", default=1, type=int, help="Manual epoch number (useful on restarts)")
 parser.add_argument("--threads", type=int, default=0, help="Number of threads for data loader to use, Default: 1")
